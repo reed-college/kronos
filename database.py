ec7f603c54e016e37f3b6379d6d924619ff584a1
jwei0701 <weij@reed.edu>
2016-05-27 13:31:03 -0700

    Update the database file.

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy import Column, Enum, ForeignKey
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -8,53 +11,100 @@ db = SQLAlchemy(app)
 
 department = ('Anthropology', 'Art', 'Biology', 'Chemistry', 'Chinese', 'Classics', 'Dance', 'Economics', 'English', 'French', 'German', 'History', 'Linguistics', 'Mathematics', 'Music', 'Philosophy', 'Physics', 'Political Science', 'Psychology', 'Religion', 'Russian', 'Sociology', 'Spanish', 'Theatre')
 division = ('The Arts', 'History and Social Sciences', 'Literature and Languages', 'Mathematics and Natural Sciences', 'Philosophy, Religion, Psychology and Linguistics')
+
+# The User class contains professors and students,
+# but that does not mean that they are the actual "users" of this website
+# (at least not for now).
  
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), unique = True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.Text)
+    name = db.Column(db.String(50), nullable = False)
+    password = db.Column(db.Text, nullable = False)
     email = db.Column(db.String(120), unique = True)
-    department = db.Column(db.Enum(*department))
-    division = db.Column(db.Enum(*division))
+    discriminator = db.Column('type', db.String(20))
+    __mapper_args__ = {'polymorphic_on': discriminator}
     
-    def __init__(self, username, name, password, email, department):
+    def __init__(self, username, name, password, email):
         self.username = username
         self.name = name
         self.password = password
         self.email = email
-        self.department = department
-        self.division = division
         
     def __repr__(self):
         return '<%r>' % self.username
-'''
-class Prof(db.User):
-    department = db.Column(db.Enum(*department))
-    division = db.Column(db.Enum(*division))
+
+class FAC(User):
+    def __init__(self, username, name, password, email):
+        User.__init__(self, username, name, password, email)
+        self.type = 'FAC'
+
+class Prof(User):    
+    department = db.Column("department", db.Enum(*department))
+    division = db.Column("division", db.Enum(*division))    
+    __mapper_args__ = {'polymorphic_identity': 'professor'}
+    id = db.Column(db.Integer, ForeignKey('user.id'), primary_key = True)
+
+    @declared_attr
+    def department(cls):
+        "Department column, if not present already."
+        return User.__table__.c.get('department', Enum(*department))
+
+    @declared_attr
+    def division(cls):
+        "Division column, if not present already."
+        return User.__table__.c.get('division', Enum(*division))
 
     def __init__(self, username, name, password, email, department, division):
-        User.__init__(self, username, name, password, email)
+        User.__init__(self, username, name, email)
         self.department = department
         self.division = division
         self.type = 'professor'
 
     def __repr__(self):
         return '<%r>' % self.username
-'''
+
+class Stu(User):
+    department = db.Column("department", db.Enum(*department))
+    division = db.Column("division", db.Enum(*division))
+    __mapper_args__ = {'polymorphic_identity': 'student'}
+    id = db.Column(db.Integer, ForeignKey('user.id'), primary_key = True)
+
+    @declared_attr
+    def department(cls):
+        "Department column, if not present already."
+        return User.__table__.c.get('department', Enum(*department))
+
+    @declared_attr
+    def division(cls):
+        "Division column, if not present already."
+        return User.__table__.c.get('division', Enum(*division))
+
+    def __init__(self, username, name, password, email, department, division):
+        User.__init__(self, username, name, email)
+        self.department = department
+        self.division = division
+        self.type = 'student'
+
+    def __repr__(self):
+        return '<%r>' % self.username
+
+
 class Event(db.Model):
+    __tablename__ = 'events'
     id = db.Column(db.String(40), primary_key = True)
     summary = db.Column(db.Text)
-    dtstart = db.Column(db.DateTime)
-    dtend = db.Column(db.DateTime)
+    dtstart = db.Column(db.DateTime, nullable = False)
+    dtend = db.Column(db.DateTime, nullable = False)
     status = db.Column(db.Enum('busy', 'free'))
     private = db.Column(db.Boolean)
-    oral = db.Column(db.Boolean)
+    discriminator = db.Column('type', db.String(10))
+    __mapper_args__ = {'polymorphic_on': discriminator}
 
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref = db.backref('event')) # lazy???
 
-    def __init__(self, summary, dtstart, dtend, user, private = True, oral = False):
+    def __init__(self, summary, dtstart, dtend, user, private = True):
         self.summary = summary
         self.dtstart = dtstart
         self.dtend = dtend
@@ -62,23 +112,30 @@ class Event(db.Model):
         self.user = user
         
     def __repr__(self):
-        return '<Event %r>' % self.summary
+        if private == False:
+            return '<Event %r>' % self.summary
+        else:
+            return '<Not available>'
 
 class Oral(db.Model):
-    __tablename__ = 'Oral'
-    id = db.Column(db.String(40), primary_key = True)
+    __tablename__ = 'orals'
+    __mapper_args__ = {'polymorphic_identity': 'oral'}
+    id = db.Column(db.String(40), ForeignKey('events.id'), primary_key = True)
     response = db.Column(db.Enum('Accepted', 'Declined', 'Tentative'))
-    
-    eventid = db.Column(db.Integer, db.ForeignKey('event.id'))
-    event = db.relationship('Event', backref = db.backref('oral'))
-    attendee_userid = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User', backref = db.backref('oral'))
 
-    def __init__(self, user, response = None):
+    prof_id = db.Column(db.Integer, db.ForeignKey('prof.id'))
+    prof = db.relationship('Prof', backref = db.backref('oral'))
+    stu_id = db.Column(db.Integer, db.ForeignKey('stu.id'))
+    stu = db.relationship('stu', backref = db.backref('oral'))
+
+    def __init__(self, stu, prof, response = None):
+        Event.__init__(self, dtstart, dtend)
         self.user = user
+        self.stu = stu
+        self.prof = prof
 
     def __repr__(self):
-        return '<Oral>' #???
+        return '<%rs Oral>' % self.stu
 
 
 
