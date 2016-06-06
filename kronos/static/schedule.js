3e0960e5187d34e99003ae98aa3471059142a8bc
alex <AlexV348@gmail.com>
2016-06-06 12:02:55 -0700

    customized the calendar a bit

--- kronos/static/schedule.js
+++ kronos/static/schedule.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-  //initializing select2
-  $("#stu-select").select2({
-    placeholder: "Students"
-  });
-  $("#prof-select").select2({
-    placeholder: "Professors"
-  });
-  //initializing fullCalendar
-  $('#calendar').fullCalendar();
+    //initializing select2
+    $("#stu-select").select2({
+        placeholder: "Students"
+    });
+    $("#prof-select").select2({
+        placeholder: "Professors"
+    });
+    //initializing fullCalendar
+    $('#calendar').fullCalendar({
+       defaultView: 'agendaWeek',         
+       weekends: false,
+       allDaySlot: false,
+       header: false,
+    });
 });
