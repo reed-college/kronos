0704626d4a166170371551e58ca10e6caf97dae7
jwei0701 <weij@reed.edu>
2016-06-29 14:38:15 -0700

    Can print time intervals when they are all available given four profs

--- kronos/scheduling.py
+++ kronos/scheduling.py
@@ -46,3 +46,22 @@ def available(user):
         i += 1
     return ls
 
+
+# Given four professors' schedule, return time intervals when they are all available.
+
+def all_avail(user1, user2, user3, user4):
+    ls = []
+    avail1 = available(user1)
+    avail2 = available(user2)
+    avail3 = available(user3)
+    avail4 = available(user4)
+    for range1 in avail1:
+        for range2 in avail2:
+            for range3 in avail3:
+                for range4 in avail4:
+                    latest_start = max(range1.start, range2.start, range3.start, range4.start)
+                    earliest_end = min(range1.end, range2.end, range3.end, range4.end)
+                    if latest_start <= earliest_end:
+                        ls.append(Range(latest_start, earliest_end))
+    return ls
+
