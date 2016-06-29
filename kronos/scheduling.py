14abda4bcfdff9aea8768cf111d74f84f3e51aee
jwei0701 <weij@reed.edu>
2016-06-29 11:12:59 -0700

    Made a function to return more human-readable results

--- kronos/scheduling.py
+++ kronos/scheduling.py
@@ -1,6 +1,8 @@
 from populatedb import *
 from kronos import db
+from collections import namedtuple
 
+Range = namedtuple('Range', ['start', 'end'])
 
 # Print the available time given a user.
 
@@ -21,6 +23,7 @@ def available(user):
         if dtend in starttimes:
             endtimes.remove(dtend)
             starttimes.remove(dtend)
+
     availstart = endtimes
     availend = starttimes
     if start < min(starttimes):
@@ -33,13 +36,13 @@ def available(user):
         availstart.remove(max(endtimes))
     availstart.sort()
     availend.sort()
-    return availstart, availend
 
-# Example:
-# A result like (
-# [datetime.datetime(2016, 5, 2, 9, 0), datetime.datetime(2016, 5, 2, 12, 0), datetime.datetime(2016, 5, 6, 19, 0)],
-# [datetime.datetime(2016, 5, 2, 10, 0), datetime.datetime(2016, 5, 6, 10, 0), datetime.datetime(2016, 5, 6, 20, 0)])
-# means that the input user is free from 20160502 9:00:00 to 20160502 10:00:00,
-# from 20160502 12:00:00 to 20160506 10:00:00,
-# and from 20160506 19:00:00 to 20160506 20:00:00.
+    i = 0
+    ls = []
+    while i < len(availstart):
+        start = availstart[i]
+        end = availend[i]
+        ls.append(Range(start, end))
+        i += 1
+    return ls
 
