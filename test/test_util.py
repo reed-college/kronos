db8ec112fcbe4f66ce03f25ae4ebb0cee3275fea
alex <AlexV348@gmail.com>
2016-06-30 09:49:25 -0700

    added test for making oral table

--- test/test_util.py
+++ test/test_util.py
@@ -1,8 +1,9 @@
 from random import randint
 import datetime
 import pytest
+from dateutil import parser
 from kronos import util
-
+from kronos.models import Oral, Stu, Prof
 
 class TestUtil:
     def test_FreeTimeCalc(self):
@@ -25,4 +26,50 @@ class TestUtil:
             if i <= 3:
                 guess.append((oralstart, oralstart + oralslength))
         assert guess == util.FreeTimeCalc(events,orals)
-            
+    
+    def test_GetOralTable(self):
+        
+        hovda = Prof('hovdap', 'Paul Hovda', 'asdf', 'hovdap@reed.edu', 'Philosophy', 'Philosophy, Religion, Psychology and Linguistics')
+        pearson = Prof('pearson', 'Pearson', 'asdf', 'pearson@reed.edu', 'Linguistics', 'Philosophy, Religion, Psychology and Linguistics')
+        emma = Stu('erennie', 'Emma Rennie', '123', 'erennie@reed.edu', 'Linguistics', 'Philosophy, Religion, Psychology and Linguistics')
+        richard = Stu('adcockr', 'Richard Adcock', 'asd', 'adcockr@reed.edu', 'Linguistics', 'Philosophy, Religion, Psychology and Linguistics')
+
+        oral1 = Oral(emma, 'Oral_Emma', parser.parse('20160502 10:00:00 AM'), parser.parse('20160502 12:00:00 PM'), pearson)
+        oral1.readers = [pearson]
+
+        oral2 = Oral(richard, 'Oral_Richard', parser.parse('20160502 03:00:00 PM'), parser.parse('20160502 05:00:00 PM'), hovda)
+        oral2.readers = [hovda]
+        
+        orals = [oral1, oral2]
+        
+        [['h', 'Monday, May 02 <br> 10:00 - 12:00'], ['c', 'Emma Rennie'], ['h', 'Monday, May 02 <br> 03:00 - 05:00'], ['c', 'Richard Adcock']]
+        
+        oraltable = util.GetOralTable(orals)
+        oral1col = None
+        oral2col = None
+        for row in oraltable:
+            # Assert that the first column of each row specifies whether its a header row or not
+            assert row[0] == 'h' or row[0] == 'c'
+            for i in range(len(row)):
+                #If this is the header for the orals at 10:00 on monday may 2nd
+                if ("Monday" in row[i] or "May 02" in row[i] or "May 2" in row[i]):
+                    if "10:00" in row[i]:
+                        oral1col = i
+                    elif "3:00" in row[i]:
+                        oral2col = i
+                elif oral1col is not None and "Emma Rennie" in row[i]:
+                    # making sure the oral is below its timeslot's header
+                    assert oral1col == i
+                    # assert the the for loop has not gotten to the second
+                    # oral header since it's later in the day
+                    assert oral2col is None
+                elif oral2col is not None and "Richard Adcock" in row[i]:
+                    # making sure the oral is below its timeslot's header
+                    assert oral2col == i
+
+
+
+
+
+
+
