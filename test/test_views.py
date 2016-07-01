d19a6063dda9c8aa4580a8b5f700cf207a904f4d
alex <AlexV348@gmail.com>
2016-07-01 10:04:34 -0700

    made view test actually assert something

--- test/test_views.py
+++ test/test_views.py
@@ -6,6 +6,12 @@ class Test_shcedule:
         kronos.app.config.from_object('kronos.config.TestConfig')
         cls.client = kronos.app.test_client()
         import populatedb
+    
+    @classmethod
+    def teardown_class(cls):
+        kronos.db.drop_all()
+
 
     def test_gives_template(self):
-        self.client.get('/')    
+        rv = self.client.get('/')    
+        assert rv.status_code == 200
