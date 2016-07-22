6d9b94798b2e95af009b41395293024786dbf075
jwei0701 <weij@reed.edu>
2016-07-22 13:37:05 -0700

    Added validators for CSV and Excel

--- kronos/importfile.py
+++ kronos/importfile.py
@@ -8,6 +8,10 @@ engine = create_engine('postgresql://Jiahui:pass@localhost/db_kronos')
 df_csv = pd.read_csv('/Users/Jiahui/kronos/kronos/csvdata.csv')
 df_csv.to_sql('pandas_db', engine, if_exists='append')
 
+assert df_csv.query('dtstart > dtend').empty
+
 # Import Excel files
 df_xlsx = pd.read_excel('/Users/Jiahui/kronos/kronos/exceldata.xlsx')
 df_xlsx.to_sql('pandas_db', engine, if_exists='append')
+
+assert df_xlsx.query('dtstart > dtend').empty
