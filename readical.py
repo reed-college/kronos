1afac625f4f55af657c39e391215c3acf099468b
jwei0701 <weij@reed.edu>
2016-07-26 17:17:45 -0700

    get a list of event summaries

--- readical.py
+++ readical.py
@@ -1,38 +1,19 @@
 from icalendar import Calendar, Event
 from datetime import datetime
+from pandas import DataFrame
 import pytz
 
 cal = Calendar() # Create a new empty calendar
-cal = cal.from_ical(open('WorkCalendar.ics','rb').read()) # Open an existing calendar
-
-
-# Display calendar in the same format as the ics file.
-def display(cal):
-    return cal.to_ical().replace('\r\n', '\n').strip()
-# To print it out:
-print display(cal)
+cal = cal.from_ical(open('USHolidays.ics','rb').read()) # Open an existing calendar
 
 # Print starting time:
 def pstart(cal):
     for event in cal.walk('vevent'):
         date = event.get('dtstart')
-        return date.to_ical()
-
-command = raw_input('Enter your oral time (YYYYMMDDHHMMSS to YYYYMMDDHHMMSS): ')
-oralstart = command[:14]
-oralend = command[18:]
-
-# Show if there is a time conflict.
-def showconflict(cal):
+    return date.to_ical()
+    
+def getsummary(cal):
+    ls = []
     for event in cal.walk('vevent'):
-        start = event.get('dtstart').to_ical()
-        end = event.get('dtend').to_ical()
-        # summary = event.get('summary').
-        if int(start[:7]) == int(oralstart[:7]):
-            if int(start[9:]) < int(oralend[8:]) and int(end[9:]) > int(oralend[8:]):
-                print("There is a time conflict.")
-            elif int(start[9:]) < int(oralstart[8:]) and int(end[9:]) > int(oralstart[8:]):
-                print("There is a time conflict.")
-
-# MIGHT need a way to print attendees' available time (using an interval tree?).
-
+        ls.append(str(event.get('summary')))
+    return ls
