3555dfd51652939087d4de3126ff975770401dd8
jwei0701 <weij@reed.edu>
2016-07-27 11:57:39 -0700

    get lists of time in datetime type

--- readical.py
+++ readical.py
@@ -11,9 +11,21 @@ def pstart(cal):
     for event in cal.walk('vevent'):
         date = event.get('dtstart')
     return date.to_ical()
-    
+
 def getsummary(cal):
     ls = []
     for event in cal.walk('vevent'):
         ls.append(str(event.get('summary')))
+    return ls
+
+def getdtstart(cal):
+    ls = []
+    for event in cal.walk('vevent'):
+        ls.append(event.get('dtstart').dt)
+    return ls
+
+def getdtend(cal):
+    ls = []
+    for event in cal.walk('vevent'):
+        ls.append(event.get('dtend').dt)
     return ls
