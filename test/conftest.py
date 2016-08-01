d72b8a8df9641c99b7cfec2c4ec5c29aa3be3951
alex <AlexV348@gmail.com>
2016-08-01 16:04:15 -0700

    made tests flake8 (except test_models)

--- test/conftest.py
+++ test/conftest.py
@@ -3,27 +3,55 @@ import kronos
 import datetime
 from kronos.models import Stu, Prof, Oral, OralStartDay
 
+
 @pytest.yield_fixture
 def setup_db(request):
-    #kronos.db.drop_all()
+    # kronos.db.drop_all()
     kronos.db.create_all()
     yield None
     pass
     kronos.db.session.remove()
     kronos.db.drop_all()
 
+
 @pytest.fixture
 @pytest.mark.usefixtures("setup_db")
 def populate_db():
-    emma = Stu('erennie', 'Emma Rennie', 'erennie@reed.edu', 'Linguistics', 'Philosophy, Religion, Psychology and Linguistics')
-    richard = Stu('adcockr', 'Richard Adcock', 'adcockr@reed.edu', 'Linguistics', 'Philosophy, Religion, Psychology and Linguistics')
-    hovda = Prof('hovdap', 'Paul Hovda', 'hovdap@reed.edu', 'Philosophy', 'Philosophy, Religion, Psychology and Linguistics')
-    hancock = Prof('hancockv', 'Ginny', 'hancockv@reed.edu', 'Music', 'The Arts')
-    oral1 = Oral(emma, 'Oral_Emma', datetime.datetime(2016,5,2,10), datetime.datetime(2016,5,2,12), emma)
+    emma = Stu(
+        'erennie',
+        'Emma Rennie',
+        'erennie@reed.edu',
+        'Linguistics',
+        'Philosophy, Religion, Psychology and Linguistics')
+    richard = Stu(
+        'adcockr',
+        'Richard Adcock',
+        'adcockr@reed.edu',
+        'Linguistics',
+        'Philosophy, Religion, Psychology and Linguistics')
+    hovda = Prof(
+        'hovdap',
+        'Paul Hovda',
+        'hovdap@reed.edu',
+        'Philosophy',
+        'Philosophy, Religion, Psychology and Linguistics')
+    hancock = Prof(
+        'hancockv',
+        'Ginny',
+        'hancockv@reed.edu',
+        'Music',
+        'The Arts')
+    oral1 = Oral(
+        emma, 'Oral_Emma', datetime.datetime(
+            2016, 5, 2, 10), datetime.datetime(
+            2016, 5, 2, 12), emma)
     oral1.readers = [hovda]
-    oral2 = Oral(richard, 'Oral_Richard', datetime.datetime(2016,5,2,15), datetime.datetime(2016,5,2,17), richard)
+    oral2 = Oral(
+        richard, 'Oral_Richard', datetime.datetime(
+            2016, 5, 2, 15), datetime.datetime(
+            2016, 5, 2, 17), richard)
     oral2.readers = [hancock]
-    s16 = OralStartDay("Sprang 2016", datetime.date(2016, 5,  2)) 
+    s16 = OralStartDay("Sprang 2016", datetime.date(2016, 5, 2))
     kronos.db.session.add(emma)
     kronos.db.session.add(richard)
     kronos.db.session.add(hovda)
@@ -38,7 +66,7 @@ def populate_db():
 def db():
     return kronos.db
 
+
 @pytest.fixture
 def client():
     return kronos.app.test_client()
-
--- test/test_util.py
+++ test/test_util.py
@@ -1,4 +1,3 @@
-from random import randint
 import datetime
 from dateutil import parser
 from kronos import util
@@ -6,7 +5,9 @@ from kronos.models import Oral, Stu, Prof, Event
 from werkzeug.datastructures import ImmutableMultiDict
 import pytest
 
+
 class TestUtil:
+
     def test_FreeTimeCalc(self):
         """
         Uses variously lengthed events and 2 hour orals placed throughout
@@ -16,48 +17,82 @@ class TestUtil:
         orals = []
         guess = []
         oralslength = datetime.timedelta(hours=2)
-        for i in range(1,6):
+        for i in range(1, 6):
             start = datetime.datetime(2017, 5, i, hour=10)
             length = datetime.timedelta(hours=i)
             events.append((start, start + length))
             oralstart = datetime.datetime(2017, 5, i, hour=13)
             orals.append((oralstart, oralstart + oralslength))
-            #This guesses which orals won't intersect the events, since
-            #the events increase in length over the week
+            # This guesses which orals won't intersect the events, since
+            # the events increase in length over the week
             if i <= 3:
                 guess.append((oralstart, oralstart + oralslength))
-        assert guess == util.FreeTimeCalc(events,orals)
-    
+        assert guess == util.FreeTimeCalc(events, orals)
+
     def test_GetOralTable(self):
         """
-        Makes sure that the oral table has header and cell tags in the 
+        Makes sure that the oral table has header and cell tags in the
         first column of each row and that the orals are placed below the
         headers for their time slots
         """
-        hovda = Prof('hovdap', 'Paul Hovda', 'hovdap@reed.edu', 'Philosophy', 'Philosophy, Religion, Psychology and Linguistics')
-        pearson = Prof('pearson', 'Pearson', 'pearson@reed.edu', 'Linguistics', 'Philosophy, Religion, Psychology and Linguistics')
-        emma = Stu('erennie', 'Emma Rennie', 'erennie@reed.edu', 'Linguistics', 'Philosophy, Religion, Psychology and Linguistics')
-        richard = Stu('adcockr', 'Richard Adcock', 'adcockr@reed.edu', 'Linguistics', 'Philosophy, Religion, Psychology and Linguistics')
+        hovda = Prof(
+            'hovdap',
+            'Paul Hovda',
+            'hovdap@reed.edu',
+            'Philosophy',
+            'Philosophy, Religion, Psychology and Linguistics')
+        pearson = Prof(
+            'pearson',
+            'Pearson',
+            'pearson@reed.edu',
+            'Linguistics',
+            'Philosophy, Religion, Psychology and Linguistics')
+        emma = Stu(
+            'erennie',
+            'Emma Rennie',
+            'erennie@reed.edu',
+            'Linguistics',
+            'Philosophy, Religion, Psychology and Linguistics')
+        richard = Stu(
+            'adcockr',
+            'Richard Adcock',
+            'adcockr@reed.edu',
+            'Linguistics',
+            'Philosophy, Religion, Psychology and Linguistics')
 
-        oral1 = Oral(emma, 'Oral_Emma', parser.parse('20160502 10:00:00 AM'), parser.parse('20160502 12:00:00 PM'), pearson)
+        oral1 = Oral(
+            emma,
+            'Oral_Emma',
+            parser.parse('20160502 10:00:00 AM'),
+            parser.parse('20160502 12:00:00 PM'),
+            pearson)
         oral1.readers = [pearson]
 
-        oral2 = Oral(richard, 'Oral_Richard', parser.parse('20160502 03:00:00 PM'), parser.parse('20160502 05:00:00 PM'), hovda)
+        oral2 = Oral(
+            richard,
+            'Oral_Richard',
+            parser.parse('20160502 03:00:00 PM'),
+            parser.parse('20160502 05:00:00 PM'),
+            hovda)
         oral2.readers = [hovda]
-        
+
         orals = [oral1, oral2]
-        
-        [['h', 'Monday, May 02 <br> 10:00 - 12:00'], ['c', 'Emma Rennie'], ['h', 'Monday, May 02 <br> 03:00 - 05:00'], ['c', 'Richard Adcock']]
-        
+
+        [['h', 'Monday, May 02 <br> 10:00 - 12:00'], ['c', 'Emma Rennie'],
+         ['h', 'Monday, May 02 <br> 03:00 - 05:00'], ['c', 'Richard Adcock']]
+
         oraltable = util.GetOralTable(orals)
         oral1col = None
         oral2col = None
         for row in oraltable:
-            # Assert that the first column of each row specifies whether its a header row or not
+            # Assert that the first column of each row specifies whether its a
+            # header row or not
             assert row[0] == 'h' or row[0] == 'c'
             for i in range(len(row)):
-                #If this is the header for the orals at 10:00 on monday may 2nd
-                if ("Monday" in row[i] or "May 02" in row[i] or "May 2" in row[i]):
+                # If this is the header for the orals at 10:00 on monday may
+                # 2nd
+                if ("Monday" in row[i] or "May 02" in row[
+                        i] or "May 2" in row[i]):
                     if "10:00" in row[i]:
                         oral1col = i
                     elif "3:00" in row[i]:
@@ -82,5 +117,3 @@ class TestUtil:
             args = ImmutableMultiDict({'professors[]': [hovdaid]})
             events = util.filter_events(Event.query, args)
             assert oral2 not in events.all()
-
-
