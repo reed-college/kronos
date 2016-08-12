d6a7fc5517bde022fbbd0855f4da9856093ba12d
jwei0701 <weij@reed.edu>
2016-08-12 14:26:06 -0700

    solved corrupted files problem

--- kronos/views.py
+++ kronos/views.py
@@ -94,17 +94,7 @@ def allowed_file(filename):
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
-        data = request.files['file'].read()
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
