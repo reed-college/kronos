eeffe1de207c193ff102df35043e72668bc1055b
jwei0701 <weij@reed.edu>
2016-08-16 11:44:22 -0700

    new migrations

---
+++ kronos/academic_constants.py
@@ -0,0 +1,10 @@
+DIVISIONS = ['The Arts', 'History and Social Sciences', 'Literature and Languages', 
+'Mathematics and Natural Sciences', 'Philosophy, Religion, Psychology, and Linguistics']
+
+THEARTS = ['Art', 'Dance', 'Music', 'Theatre']
+HSS = ['Anthropology', 'Economics', 'History', 'Political Science', 'Sociology']
+LL = ['American Studies', 'Chinese', 'Classics', 'English', 'Creative Writing', 'French', 
+'General LIterature', 'German', 'Russian', 'Spanish']
+MNS = ['Biology', 'Chemistry', 'Mathematics', 'Physics']
+PRPL = ['Linguistics', 'Philosophy', 'Psychology', 'Religion']
+DEPARTMENTS = THEARTS + HSS + LL + MNS + PRPL
